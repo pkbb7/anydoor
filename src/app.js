f3d332638389d4d95301f22a8a482219252ba4e8
pkbb7 <pkbb7@sina.cn>
2019-09-21 15:19:57 +0800

    简易文件服务器

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const chalk = require('chalk');
+const path  = require('path');
+const config = require('./config/defaultConfig');
+const route = require("./helper/route");
+
+const server = http.createServer((req, res)=>{
+	const filePath = path.join(config.root, req.url);
+	route(req, res, filePath);
+});
+
+server.listen(config.port, config.hostname, ()=>{
+	const addr = `http://${config.hostname}:${config.port}`;
+	console.info(`Server started at ${chalk.green(addr)}`);
+});
